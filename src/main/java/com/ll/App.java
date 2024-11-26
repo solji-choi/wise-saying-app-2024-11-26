ac9f126f59c64c1c88b18c1a66ebe9000fce0041
solji-choi <solji.choi89@gmail.com>
2024-11-26 15:23:20 +0900

    WiseSayingController 추가

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,16 +1,19 @@
 package com.ll;
 
 import com.ll.domain.system.controller.controller.SystemController;
+import com.ll.wiseSaying.controller.WiseSayingController;
 
 import java.util.Scanner;
 
 public class App {
     private final Scanner scanner;
     private final SystemController systemController;
+    private final WiseSayingController wiseSayingController;
 
     public App(Scanner scanner) {
         this.scanner = scanner;
         this.systemController = new SystemController();
+        this.wiseSayingController = new WiseSayingController(scanner);
     }
     public void run() {
         System.out.println("== 명언 앱 ==");
@@ -24,8 +27,7 @@ public class App {
 
                 break;
             } else if(cmd.equals("등록")) {
-                System.out.println("명언 : ");
-                System.out.println("작가 : ");
+                wiseSayingController.actionAdd();
             }
 
         }
