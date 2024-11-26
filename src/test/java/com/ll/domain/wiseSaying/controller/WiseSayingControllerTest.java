a22ea602b0582178fd085c35ff7457d3f18488e3
solji-choi <solji.choi89@gmail.com>
2024-11-26 15:25:18 +0900

    test 리팩토링

--- src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
+++ src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -8,38 +8,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class WiseSayingControllerTest {
     @Test
-    @DisplayName("== 명언 앱 ==")
-    public void t1() {
-        String output = AppTest.run("");
-
-        assertThat(output)
-                .contains("== 명언 앱 ==");
-    }
-
-    @Test
-    @DisplayName("명령) ")
-    public void t2() {
-        String output = AppTest.run("""
-                목록
-                """);
-
-        assertThat(output)
-                .contains("명령) ");
-    }
-
-    @Test
-    @DisplayName("명령을 2번 이상 입력할 수 있습니다.")
-    public void t3() {
-        String output = AppTest.run("""
-                목록
-                목록
-                """);
-
-        String[] split = output.split("명령\\)");
-        assertThat(split).hasSize(4);
-    }
-
-    @Test
     @DisplayName("등록을 입력하면 내용과 작가를 입력받는다.")
     public void t4() {
         String output = AppTest.run("""
