2076da2daa21036cbedddcef45113c07c5893730
solji-choi <solji.choi89@gmail.com>
2024-11-26 12:25:03 +0900

    사람이 만든 것

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,7 +1,18 @@
 package com.ll;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        lab1();
+    }
 
+    private static void lab1() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("명령) ");
+        String cmd = scanner.nextLine().trim();
+
+        System.out.println("입력한 명령 : " + cmd);
     }
 }
