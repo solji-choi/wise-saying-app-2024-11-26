a8608b68b22183a4a8579f7e508f8ecab0dd0496
solji-choi <solji.choi89@gmail.com>
2024-11-26 12:29:52 +0900

    로봇이 대신 쓰게끔

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,14 +1,27 @@
 package com.ll;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        lab1();
+        //lab1();
+        lab2();
     }
 
     private static void lab1() {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("명령) ");
+        String cmd = scanner.nextLine().trim();
+
+        System.out.println("입력한 명령 : " + cmd);
+    }
+
+    private static void lab2() {
+        InputStream in = new ByteArrayInputStream("메롱".getBytes());
+        Scanner scanner = new Scanner(in);
 
         System.out.print("명령) ");
         String cmd = scanner.nextLine().trim();
