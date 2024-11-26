b0cd6a7422cd0412e87c7188f85ade2b9a00ca54
solji-choi <solji.choi89@gmail.com>
2024-11-26 14:58:36 +0900

    매번 반복되는 종료 명령어 처리

--- src/test/java/com/ll/AppTest.java
+++ src/test/java/com/ll/AppTest.java
@@ -7,13 +7,17 @@ import java.util.Scanner;
 
 public class AppTest {
     public static String run(String input) {
-        input = input.stripIndent().trim();
+        input = input.stripIndent().trim() + "\n종료";
         Scanner scanner = TestUtil.getScanner(input);
         ByteArrayOutputStream outputStream = TestUtil.setOutToByteArray();
+
         App app = new App(scanner);
         app.run();
+
         String output = outputStream.toString();
+
         TestUtil.clearSetOutToByteArray(outputStream);
+
         return output;
     }
 }
--- src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
+++ src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -10,7 +10,7 @@ public class WiseSayingControllerTest {
     @Test
     @DisplayName("== 명언 앱 ==")
     public void t1() {
-        String output = AppTest.run("종료");
+        String output = AppTest.run("");
 
         assertThat(output)
                 .contains("== 명언 앱 ==");
@@ -21,7 +21,6 @@ public class WiseSayingControllerTest {
     public void t2() {
         String output = AppTest.run("""
                 목록
-                종료
                 """);
 
         assertThat(output)
@@ -34,7 +33,6 @@ public class WiseSayingControllerTest {
         String output = AppTest.run("""
                 목록
                 목록
-                종료
                 """);
 
         String[] split = output.split("명령\\)");
