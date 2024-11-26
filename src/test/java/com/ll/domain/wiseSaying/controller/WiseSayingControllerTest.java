11b36614141c291ed5dcce4b48efe0254838a3e3
solji-choi <solji.choi89@gmail.com>
2024-11-26 15:14:57 +0900

    등록기능 추가 테스트(실패)

--- src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
+++ src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -38,4 +38,18 @@ public class WiseSayingControllerTest {
         String[] split = output.split("명령\\)");
         assertThat(split).hasSize(4);
     }
+
+    @Test
+    @DisplayName("등록을 입력하면 내용과 작가를 입력받는다.")
+    public void t4() {
+        String output = AppTest.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                """);
+
+        assertThat(output)
+                .contains("명언 : ")
+                .contains("작기 : ");
+    }
 }
