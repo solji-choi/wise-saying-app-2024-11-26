8a54e550772d9876577a93394d9d1c4a4db06733
solji-choi <solji.choi89@gmail.com>
2024-11-26 14:45:31 +0900

    실패하는 소스 생성

--- src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
+++ src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -27,4 +27,17 @@ public class WiseSayingControllerTest {
         assertThat(output)
                 .contains("명령) ");
     }
+
+    @Test
+    @DisplayName("명령을 2번 이상 입력할 수 있습니다.")
+    public void t3() {
+        String output = AppTest.run("""
+                목록
+                목록
+                종료
+                """);
+
+        String[] split = output.split("명령\\) ");
+        assertThat(split).hasSize(4);
+    }
 }
