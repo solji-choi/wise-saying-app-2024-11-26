396b08514d50fbb2eeac39c3057ffa4c02c7f743
solji-choi <solji.choi89@gmail.com>
2024-11-26 14:08:30 +0900

    TestUtil 클래스 테스트

--- src/test/java/com/ll/standard/util/TestUtilTest.java
+++ src/test/java/com/ll/standard/util/TestUtilTest.java
@@ -12,9 +12,9 @@ public class TestUtilTest {
     @DisplayName("TestUtil.getScanner()")
     public void t1() {
         Scanner scanner = TestUtil.getScanner("""
-                 등록
-                 나의 죽음을 적들에게 알리지 마라!
-                 이순신
+                등록
+                나의 죽음을 적들에게 알리지 말라!
+                이순신
                 """.stripIndent().trim());
 
         String cmd = scanner.nextLine();
@@ -22,7 +22,7 @@ public class TestUtilTest {
         String author = scanner.nextLine();
 
         assertThat(cmd).isEqualTo("등록");
-        assertThat(cmd).isEqualTo("나의 죽음을 적들에게 알리지 마라!");
-        assertThat(cmd).isEqualTo("이순신");
+        assertThat(content).isEqualTo("나의 죽음을 적들에게 알리지 말라!");
+        assertThat(author).isEqualTo("이순신");
     }
 }
