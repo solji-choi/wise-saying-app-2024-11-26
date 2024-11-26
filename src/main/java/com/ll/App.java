9132e8d9bf5b7b15c5707e216206b7f0fa3bbc78
solji-choi <solji.choi89@gmail.com>
2024-11-26 14:56:48 +0900

    systemController 도입

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,11 +1,16 @@
 package com.ll;
 
+import com.ll.domain.system.controller.controller.SystemController;
+
 import java.util.Scanner;
 
 public class App {
     private final Scanner scanner;
+    private final SystemController systemController;
+
     public App(Scanner scanner) {
         this.scanner = scanner;
+        this.systemController = new SystemController();
     }
     public void run() {
         System.out.println("== 명언 앱 ==");
@@ -15,6 +20,8 @@ public class App {
             String cmd = scanner.nextLine();
 
             if(cmd.equals("종료")) {
+                systemController.actionExit();
+
                 break;
             }
 
