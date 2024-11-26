f2a28eb794914edec5b014fd094c475c6aee3665
solji-choi <solji.choi89@gmail.com>
2024-11-26 14:25:04 +0900

    App 클래스 생성

---
+++ src/main/java/com/ll/App/App.java
@@ -0,0 +1,13 @@
+package com.ll.App;
+
+import java.util.Scanner;
+
+public class App {
+    private final Scanner scanner;
+    public App(Scanner scanner) {
+        this.scanner = scanner;
+    }
+    public void run() {
+        System.out.println("== 명언 앱 ==");
+    }
+}
