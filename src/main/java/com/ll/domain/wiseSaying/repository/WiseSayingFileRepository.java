6dc408c22b197b20a4fa532534096b1cdc183e73
solji-choi <solji.choi89@gmail.com>
2024-11-27 22:57:47 +0900

    WiseSayingFileRepository 생성

---
+++ src/main/java/com/ll/domain/wiseSaying/repository/WiseSayingFileRepository.java
@@ -0,0 +1,41 @@
+package com.ll.domain.wiseSaying.repository;
+
+import com.ll.domain.wiseSaying.entity.WiseSaying;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class WiseSayingFileRepository extends WiseSayingRepository {
+    private final List<WiseSaying> wiseSayings;
+    private int lastId;
+
+    public WiseSayingFileRepository() {
+        this.wiseSayings = new ArrayList<>();
+        this.lastId = 0;
+    }
+
+    public WiseSaying save(WiseSaying wiseSaying) {
+        if (!wiseSaying.isNew()) {
+            return wiseSaying;
+        }
+
+        wiseSaying.setId(++lastId);
+        wiseSayings.add(wiseSaying);
+        return wiseSaying;
+    }
+
+    public List<WiseSaying> findAll() {
+        return wiseSayings;
+    }
+
+    public boolean deleteById(int id) {
+        return wiseSayings.removeIf(wiseSaying -> wiseSaying.getId() == id);
+    }
+
+    public Optional<WiseSaying> findById(int id) {
+        return wiseSayings.stream()
+                .filter(wiseSaying -> wiseSaying.getId() == id)
+                .findFirst();
+    }
+}
