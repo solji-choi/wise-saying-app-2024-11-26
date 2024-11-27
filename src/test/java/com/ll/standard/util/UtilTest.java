45a0578031528a60bfce566e1b2edfafb7d84ab6
solji-choi <solji.choi89@gmail.com>
2024-11-27 16:52:37 +0900

    파일 생í성할 시 해당 경로의 폴더가 없다면 만드는 기í능 테스트ã(실패)

--- src/test/java/com/ll/standard/util/UtilTest.java
+++ src/test/java/com/ll/standard/util/UtilTest.java
@@ -58,4 +58,18 @@ public class UtilTest {
                 Util.file.notExists(filePath)
         ).isTrue();
     }
+
+    @Test
+    @DisplayName("파일을 생성할 수 있다, 만약 해당 경로의 폴더가 없다면 만든다.")
+    public void t4() {
+        String filePath = "temp/temp/test.txt";
+
+        Util.file.touch(filePath);
+
+        assertThat(
+                Util.file.exists(filePath)
+        ).isTrue();
+
+        Util.file.delete(filePath);
+    }
 }
