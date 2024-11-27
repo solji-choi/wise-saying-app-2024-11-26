39e6d501e754118a7a5b3f97964d84f3b9c73561
solji-choi <solji.choi89@gmail.com>
2024-11-27 17:19:46 +0900

    WiseSaying  클래스 모든 필드 getter, setter 처리

--- src/main/java/com/ll/domain/wiseSaying/entity/WiseSaying.java
+++ src/main/java/com/ll/domain/wiseSaying/entity/WiseSaying.java
@@ -8,7 +8,13 @@ public class WiseSaying {
     @Getter
     @Setter
     private int id;
+
+    @Getter
+    @Setter
     private String content;
+
+    @Getter
+    @Setter
     private String author;
 
     public WiseSaying(int id, String content, String author) {
@@ -19,21 +25,5 @@ public class WiseSaying {
 
     public boolean isNew() {
         return id == 0;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
     }
 }
