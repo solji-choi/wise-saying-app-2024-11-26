8eaedc55c6232672e8f1ed88593a49990c41f3fa
solji-choi <solji.choi89@gmail.com>
2024-11-27 10:09:42 +0900

    Command 기능 테스트(실패)

---
+++ src/test/java/com/ll/CommandTest.java
@@ -0,0 +1,13 @@
+package com.ll;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CommandTest {
+    @Test
+    public void t1() {
+        Command cmd = new Command("삭제?id=10");
+        assertThat(cmd.getActionName()).isEqualTo("삭제");
+    }
+}
