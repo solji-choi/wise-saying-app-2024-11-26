8628f5829490ba1cba0588a128f41f87010ff826
solji-choi <solji.choi89@gmail.com>
2024-11-27 22:50:45 +0900

    JSON을 Map으로 만들기 (논리 테스트)(성공)

--- src/test/java/com/ll/standard/util/JsonUtilTest.java
+++ src/test/java/com/ll/standard/util/JsonUtilTest.java
@@ -176,4 +176,27 @@ public class JsonUtilTest {
                 .containsEntry("gender", "남자")
                 .containsEntry("height", 178.1543221);
     }
+
+    @Test
+    @DisplayName("JSON to Map(숫자필드(실수))")
+    public void t10() {
+        String jsonStr = """
+                {
+                    "id": 1,
+                    "name": "이름",
+                    "gender": "남자",
+                    "height": 178.1543221,
+                    "married": false
+                }
+                """.stripIndent().trim();
+
+        Map<String, Object> map = Util.json.toMap(jsonStr);
+
+        assertThat(map)
+                .containsEntry("id", 1)
+                .containsEntry("name", "이름")
+                .containsEntry("gender", "남자")
+                .containsEntry("height", 178.1543221)
+                .containsEntry("married", false);
+    }
 }
