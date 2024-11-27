c064c4d4058d7c9ba1f5c5b292d0cefeb5638b11
solji-choi <solji.choi89@gmail.com>
2024-11-27 09:45:26 +0900

    존재하지 않는 명언에 대한 삭제 처리 기능 테스트(실패)

--- src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
+++ src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -113,4 +113,22 @@ public class WiseSayingControllerTest {
                 .contains("2 / 작자미상 / 과거에 집착하지 마라.")
                 .doesNotContain("1 / 작자미상 / 현재를 사랑하라.");
     }
+
+    @Test
+    @DisplayName("삭제 명령어 : 존재하지 않는 명언번호에 대한 처리")
+    public void t10() {
+        String output = AppTest.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                등록
+                과거에 집착하지 마라.
+                작자미상
+                삭제?id=3
+                목록
+                """);
+
+        assertThat(output)
+                .contains("1번 명언은 존재하지 않습니다.");
+    }
 }
