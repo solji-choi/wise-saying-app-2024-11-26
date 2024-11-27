94cd0158ed43839f26d2ee58ccaaf2729898d232
solji-choi <solji.choi89@gmail.com>
2024-11-27 17:22:57 +0900

    WiseSaying 생성자 롬복으로 처리

--- src/main/java/com/ll/domain/wiseSaying/entity/WiseSaying.java
+++ src/main/java/com/ll/domain/wiseSaying/entity/WiseSaying.java
@@ -1,22 +1,17 @@
 package com.ll.domain.wiseSaying.entity;
 
 import com.ll.wiseSaying.controller.WiseSayingController;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+@AllArgsConstructor
 @Getter
 @Setter
 public class WiseSaying {
-
     private int id;
     private String content;
     private String author;
-
-    public WiseSaying(int id, String content, String author) {
-        this.id = id;
-        this.content = content;
-        this.author = author;
-    }
 
     public boolean isNew() {
         return id == 0;
