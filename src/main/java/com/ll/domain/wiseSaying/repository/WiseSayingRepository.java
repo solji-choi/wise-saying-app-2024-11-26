fb8dc0bffd2919df5612ca32e2df8d4264976ec3
solji-choi <solji.choi89@gmail.com>
2024-11-27 09:33:59 +0900

    WiseSayingRepository 추가 및 리팩토링

---
+++ src/main/java/com/ll/domain/wiseSaying/repository/WiseSayingRepository.java
@@ -0,0 +1,26 @@
+package com.ll.domain.wiseSaying.repository;
+
+import com.ll.domain.wiseSaying.entity.WiseSaying;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WiseSayingRepository {
+    private final List<WiseSaying> wiseSayings;
+    private int lastId;
+
+    public WiseSayingRepository() {
+        this.wiseSayings = new ArrayList<>();
+        this.lastId = 0;
+    }
+
+    public WiseSaying save(WiseSaying wiseSaying) {
+        wiseSaying.setId(++lastId);
+        wiseSayings.add(wiseSaying);
+        return wiseSaying;
+    }
+
+    public List<WiseSaying> findAll() {
+        return wiseSayings;
+    }
+}
--- src/main/java/com/ll/domain/wiseSaying/service/WiseSayingService.java
+++ src/main/java/com/ll/domain/wiseSaying/service/WiseSayingService.java
@@ -1,26 +1,27 @@
 package com.ll.domain.wiseSaying.service;
 
 import com.ll.domain.wiseSaying.entity.WiseSaying;
+import com.ll.domain.wiseSaying.repository.WiseSayingRepository;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class WiseSayingService {
-    private final List<WiseSaying> wiseSayings;
-    private int lastId;
+    private final WiseSayingRepository wiseSayingRepository;
 
     public WiseSayingService() {
-        this.wiseSayings = new ArrayList<>();
-        this.lastId = 0;
+        this.wiseSayingRepository = new WiseSayingRepository();
     }
 
     public WiseSaying add(String content, String author) {
-        WiseSaying wiseSaying = new WiseSaying(++lastId, content, author);
-        wiseSayings.add(wiseSaying);
+        WiseSaying wiseSaying = new WiseSaying(0, content, author);
+
+        wiseSayingRepository.save(wiseSaying);
+
         return wiseSaying;
     }
 
     public List<WiseSaying> findAll() {
-        return wiseSayings;
+        return wiseSayingRepository.findAll();
     }
 }
