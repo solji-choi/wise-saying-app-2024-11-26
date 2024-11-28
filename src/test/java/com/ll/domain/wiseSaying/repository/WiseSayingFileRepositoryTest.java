17b72bbe925387fefbd26cf44696b74e81104e62
solji-choi <solji.choi89@gmail.com>
2024-11-28 10:49:39 +0900

    findAll(r)

--- src/test/java/com/ll/domain/wiseSaying/repository/WiseSayingFileRepositoryTest.java
+++ src/test/java/com/ll/domain/wiseSaying/repository/WiseSayingFileRepositoryTest.java
@@ -64,4 +64,18 @@ public class WiseSayingFileRepositoryTest {
                 opWiseSaying.get()
         ).isEqualTo(wiseSaying);
     }
+
+    @Test
+    @DisplayName("명언 다건조회")
+    public void t4() {
+        WiseSaying wiseSaying1 = new WiseSaying(0, "꿈을 지녀라. 그러면 어려운 현실을 이길 수 있다.", "괴테");
+        wiseSayingRepository.save(wiseSaying1);
+
+        WiseSaying wiseSaying2 = new WiseSaying(0, "나의 삶의 가치는 나의 결정에 달려있다.", "아인슈타인");
+        wiseSayingRepository.save(wiseSaying2);
+
+        assertThat(
+                wiseSayingRepository.findAll()
+        ).containsExactlyInAnyOrder(wiseSaying1, wiseSaying2);
+    }
 }
